69e24aa404abf52f738f4411eedcca6ac25c877b
Masakaz Ozaki <masakaz@ieee.org>
2023-08-17 10:38:04 +0900

    add for visionos

--- scripts/build_podspec.sh
+++ scripts/build_podspec.sh
@@ -80,6 +80,7 @@ Pod::Spec.new do |s|
   s.osx.deployment_target = '10.9'
   s.tvos.deployment_target = '9.0'
   s.watchos.deployment_target = '2.0'
+  s.visionos.deployment_target = '1.0'
 
   s.source_files = 'Sources/Logging/**/*.swift'
 end
